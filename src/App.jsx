3ba65d8b4d210945ccf4b6fd054400cf885db4c8
nadiaputrirah <nadiapputriirah@gmail.com>
2025-11-02 20:26:01 +0700

    [Feature] Slicing Page – Forum Diskusi (8): implement forum discussion pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Navbar from "./view/components/layout/Navbar";
 import Footer from "./view/components/layout/Footer";
 import Home from "./view/pages/Home";
 import Login from "./view/pages/Login";
+import Forum from "./view/pages/Forum";
+import ForumNew from "./view/components/layout/forum/ForumNew";
+import ForumDetail from "./view/components/layout/forum/ForumDetail";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/forum/new" element={<ForumNew />} />
+            <Route path="/forum/:id" element={<ForumDetail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
